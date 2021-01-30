500e56269031e64e289c1c02d2e25b1e3854ff35
Julien Bernard <julien.bernard@univ-fcomte.fr>
2021-01-30 14:52:01 +0100

    add first dialogs

--- code/bits/Plot.cc
+++ code/bits/Plot.cc
@@ -9,12 +9,25 @@ namespace tlw {
   : m_game(game)
   {
     // TODO: remove this
-    m_game.state.currentDialog = "Chap1DreadFirstMeeting"_id;
+    m_game.state.currentDialog = "Chap1Dread1"_id;
   }
 
   void Plot::onDialogEnd(gf::Id dialogId) {
     switch (dialogId) {
-      case "Chap1DreadFirstMeeting"_id:
+      case "Chap1Dread1"_id:
+        m_game.state.currentDialog = "Chap1Elders1"_id;
+        break;
+
+      case "Chap1Elders1"_id:
+        m_game.state.currentDialog = "Chap1Dread_NotFinished"_id;
+        break;
+
+      case "Chap1Dread_NotFinished"_id:
+        m_game.state.currentDialog = "Chap1Dread_Finished"_id;
+        break;
+
+      case "Chap1Dread_Finished"_id:
+        m_game.state.currentDialog = "Chap1Dread1"_id;
         break;
 
       default:
